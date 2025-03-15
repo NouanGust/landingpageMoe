42a61d22e927935f7fc4f578f5fd7a9452a5e220
Nohan Gustavo <nohan.gnb@gmail.com>
2025-03-14 22:05:47 -0300

    atualização do index

--- main.js
+++ main.js
@@ -2,16 +2,46 @@ document.addEventListener("DOMContentLoaded", function () {
     carregarProdutos();
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("produtos.json").then(response => response.json()).then(produtos=> {
+        const container = document.getElementById("produtos-container")
+        container.innerHTML = ""
+
+        produtos.slice(0,3).forEach(produto => {
+            const produtoHTML = `
+                <div class="col-md-3">
+                    <div class="card">
+                        <img src="${produto.estampa}" class="card-img-top" alt="${produto.nome}">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">${produto.nome}</h5>
+                            <p class="card-text">${produto.colecao}</p>
+                            <a href="detalhes.html?produto=${produto.nome} "class="btn btn-primary mt-2">Ver</a>
+                        </div>
+                    </div>
+                </div>
+            `;
+            container.innerHTML += produtoHTML;
+        })
+    }).catch(error => console.log("Erro ao carregar produtos: ", error))
+
+
+});
+
 // Carrega os produtos do JSON e exibe na página
 function carregarProdutos() {
     fetch("produtos.json")
-        .then(response => response.json())
-        .then(produtos => {
-            exibirProdutos(produtos);
-            configurarFiltro(produtos);
-        })
-        .catch(error => console.error("Erro ao carregar os produtos:", error));
-}
+    .then(response => response.json())
+    .then(produtos => {
+        exibirProdutos(produtos);
+        configurarFiltro(produtos);
+    })
+    .catch(error => console.error("Erro ao carregar os produtos:", error));
+};
+
+
+
+
+
 
 // Exibe os produtos na tela
 function exibirProdutos(produtos) {
